a6dc28cbd0346a3db3321c80ba295c3529954b8a
TravelerLee <Lc19941202Lc@gmail.com>
2021-03-08 20:32:48 +0800

    学习了list常用方法的使用

--- DartFlutter学习记录/List_Study.dart
+++ DartFlutter学习记录/List_Study.dart
@@ -1,3 +1,15 @@
 List_Study
 
 常用属性
+length  list的长度，返回int
+reversed   将list反转
+isEmpty 是否为空，返回bool
+isNotEmpty 是否不为空，返回bool
+
+常用方法：
+
+add 增加一个list参数
+addAll 拼接数组
+indexOf 查找索引 传入具体值
+remove  删除  传入具体值
+removeAt 删除 传入索引值
