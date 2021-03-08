a2b5b081fda37469d768a2fc7572bf8b255b08a0
TravelerLee <Lc19941202Lc@gmail.com>
2021-03-08 21:11:21 +0800

    添加初步git使用方法

---
+++ DartFlutter学习记录/git 常用操作方法.dart
@@ -0,0 +1,15 @@
+git 常用操作方法
+
+1、切换至仓库目录
+
+仓库Repository
+先将仓库目录初始化，会生成.git目录
+git init
+
+git init newRepository 会生成newRepository文件，并在目录下初始化，生成.git目录
+
+克隆拷贝仓库
+在github：进入别人的库点击fork。
+
+本地：git clone url  
+    例如：git clone git@github.com:TravelerLee/Study-Diary.git
