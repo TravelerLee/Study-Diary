2c8211b7fc42cf8714e9220e6bc3d3eee6c478c6
TravelerLee <Lc19941202Lc@gmail.com>
2021-03-18 17:24:03 +0800

    git问题及处理办法

---
+++ DartFlutter学习记录/github问题及处理.dart
@@ -0,0 +1,5 @@
+/**
+ * 1.  OpenSSL SSL_connect: Connection was reset in connection to github.com:443
+ * git clone 下载时报错：OpenSSL SSL_connect: Connection was reset in connection to github.com:443
+      解决：git config --global http.sslVerify false
+ */
